f59e7ab05e3a01fd6ac55ce6ececb603400e7c05
alfanpratama22 <124083508+alfanpratama22@users.noreply.github.com>
2023-02-01 03:26:28 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var sections = $('section'),
+    nav = $('nav'),
+    nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function () {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
+
+            $(this).addClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+    });
+});
+
+nav.find('a').on('click', function () {
+    var $el = $(this),
+        id = $el.attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - nav_height
+    }, 600);
+
+    return false;
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+});
